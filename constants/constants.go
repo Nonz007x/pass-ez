3f6dcc6abe61efb7f4738420e9f6c6e87aba517d
unknown <comandor969@gmail.com>
2024-05-05 22:06:52 +0700

    change file extension

--- constants/constants.go
+++ constants/constants.go
@@ -7,15 +7,15 @@ const DEFAULT_ITERATION = 100000
 const DEFAULT_SALT_LEN = 32
 const DEFAULT_KEY_LEN = 32
 
-const ENC_FILE_EXT = ".jenc"
+const ENC_FILE_EXT = ".json"
 const PARAMS_FILE_EXT = ".params"
 
 type Service struct {
-	ServiceName     string    `json:"serviceName"`
+	ServiceName string       `json:"serviceName"`
 	Credentials []Credential `json:"credentials"`
 }
 
 type Credential struct {
-	Username string `json:"username"`
-	Password []byte `json:"password"`
+	Identifier string `json:"identifier"`
+	Password   []byte `json:"password"`
 }
